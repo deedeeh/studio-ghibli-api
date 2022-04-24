917bbef2d7e65a50e1c6e3f343da2a4f90f20d75
deedeeh <dinahafez.8@gmail.com>
2022-04-24 16:32:46 +0100

    Fetched the correct data from Ghibli Api and displayed it in App - handled response data with useState and useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ movie, setMovie ] = useState<string>('');
+
+  useEffect(() => {
+    getMovie('2baf70d1-42bb-4437-b551-e5fed5a87abe');
+  }, []);
+
+  const getMovie = async (movieId: string) => {
+    try {
+      const response = await axios(`https://ghibliapi.herokuapp.com/films/${movieId}`);
+      setMovie(response.data.title);
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="App">
-      
+      <h1 className='App-header'>{movie}</h1>
     </div>
   );
 }
