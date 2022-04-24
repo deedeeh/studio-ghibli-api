163299aefdf80ad251d28c0faf15a9b58c61835a
deedeeh <dinahafez.8@gmail.com>
2022-04-24 19:25:05 +0100

    Mocked API call with Mock Service Worker library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node'
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import App from './App';
 
-test('renders App with the correct movie title', () => {
-  
+const server = setupServer(
+  rest.get('https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe', (req, res, ctx) => {
+    return res(ctx.json({title: 'Castle in the Sky'}));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('loads and displays movie', async () => {
+  render(<App />);
+  fireEvent.click(screen.getByText('Load movie'));
+  await screen.findByRole('heading');
+  expect(screen.getByRole('heading')).toBeInTheDocument();
+  expect(screen.getByRole('heading')).toHaveTextContent('Castle in the Sky');
 });
--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
 
   const [ movie, setMovie ] = useState<string>('');
+  const [buttonClicked, setButtonClicked] = useState(false);
 
   useEffect(() => {
     getMovie('2baf70d1-42bb-4437-b551-e5fed5a87abe');
@@ -19,9 +20,19 @@ function App() {
     }
   }
 
+  const handleOnClick = () => {
+    getMovie('2baf70d1-42bb-4437-b551-e5fed5a87abe');
+    setButtonClicked(true);
+  }
+
   return (
     <div className="App">
-      <h1 className='App-header'>{movie}</h1>
+      {!buttonClicked && <div className='App-button'>
+        <button onClick={handleOnClick}>
+          Load movie
+        </button>
+      </div>}
+    {buttonClicked && movie.length > 0 && <h1 className='App-header'>{movie}</h1>}
     </div>
   );
 }
